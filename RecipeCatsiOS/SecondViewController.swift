a3f13a9ffee1471c8b5a484161ae58ce7cca649b
FumikoIshizawa <yb_kos.pecotrue@docomo.ne.jp>
2016-01-13 14:20:00 +0900

    Initial Commit

---
+++ RecipeCatsiOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RecipeCatsiOS
+//
+//  Created by FumikoIshizawa on 2016/01/13.
+//  Copyright © 2016年 fumikoi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
